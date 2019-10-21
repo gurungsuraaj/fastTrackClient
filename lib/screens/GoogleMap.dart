a94af80a478fba4d2d2356355ac07a9708966b3b
Suraj Gurung <gurungsooraj21@gmail.com>
2019-10-21 17:15:50 +0545

    Login fixes

--- lib/screens/GoogleMap.dart
+++ lib/screens/GoogleMap.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:fasttrackgarage_app/utils/AppBarWithTitle.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapActivity extends StatefulWidget {
@@ -12,21 +11,10 @@ class GoogleMapActivity extends StatefulWidget {
 
 class _GoogleMapState extends State<GoogleMapActivity> {
   Completer<GoogleMapController> _controller = Completer();
-  static double lat;
-  static double long;
-  LatLng _center ;
 
   @override
   void initState() {
     super.initState();
-    getCurrentLocation();
-   /* getUserLocation().then((val) {
-      lat = val.latitude.toDouble();
-      long = val.longitude.toDouble();
-      debugPrint("This is latitude : $lat");
-      debugPrint("This is longitude : $long");
-
-    });*/
   }
 
   GoogleMapController mapController;
@@ -40,15 +28,15 @@ class _GoogleMapState extends State<GoogleMapActivity> {
           Container(
             width: double.infinity,
             height: double.infinity,
-            child: lat != null ? GoogleMap(
-              initialCameraPosition: CameraPosition(
+            child: GoogleMap(
+              initialCameraPosition: const CameraPosition(
                 bearing: 270.0,
-                target: LatLng(lat,long),
+                target: LatLng(28.2096, 83.9856),
                 zoom: 17.0,
               ),
               onMapCreated: _onMapCreated,
-              markers: {outletLocation}
-            ) :  Container(),
+              markers: {outletLocation},
+            ),
           ),
         ],
       ),
@@ -56,48 +44,16 @@ class _GoogleMapState extends State<GoogleMapActivity> {
   }
 
   void _onMapCreated(GoogleMapController controller) {
-    debugPrint("This is the value of lat : $lat");
     setState(() {
       mapController = controller;
     });
   }
   Marker outletLocation = Marker(
   markerId: MarkerId('Pokhara'),
-  position: lat != null ? LatLng(lat, long) : LatLng(28.2096, 83.9856), //TODO get marker
+  position: LatLng(28.2096, 83.9856),
+  infoWindow: InfoWindow(title: 'Bur Dubai, mankhod road'),
   icon: BitmapDescriptor.defaultMarkerWithHue(
     BitmapDescriptor.hueRed,
   ),
 );
-
-  void getCurrentLocation() async {
-    var currentLocation = LocationData;
-
-    var location = new Location();
-
-    location.getLocation().then((val) {
-      setState(() {
-        lat = val.latitude;
-        long = val.longitude;
-      });
-    });
-
- /*   location.onLocationChanged().listen((LocationData currentLocation) {
-      print(currentLocation.latitude);
-      print(currentLocation.longitude);
-    });*/
-  }
-
-/*
-  Future<Position> locateUser() async {
-    return Geolocator()
-        .getCurrentPosition();
-  }
-
-  Future<LatLng> getUserLocation() async {
-    currentLocation = await locateUser();
-    setState(() {
-      _center = LatLng(currentLocation.latitude, currentLocation.longitude);
-    });
-    return _center;
-  }*/
 }
