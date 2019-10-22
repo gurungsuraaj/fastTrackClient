a89bd30f719fa04feceb5dc60c724c79abeda4a0
Suraj <this.surajdharel@gmail.com>
2019-10-22 10:11:27 +0545

    working on shop and go

--- lib/api/Api.dart
+++ lib/api/Api.dart
@@ -6,6 +6,7 @@ class Api{
   static final String POST_CUSTOMER_SIGNUP = "$URL//api/FastrackPostApi/CustomerSignUp";
   static final String POST_CUSTOMER_LOGIN = "$URL//api/FastrackPostApi/Customerlogin";
   static final String LOCCATION_LIST = "$URL/api/FastrackPostApi/LocationList";
+  static final String SEARCH_ITEM = "$URL/api/FastrackPostApi/ItemListSearch";
 
 
 }
