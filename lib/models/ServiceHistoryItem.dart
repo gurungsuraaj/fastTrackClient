42be5e9568f838a337d39c0510f481120c4519af
sarvagya <shresthasarvagya@gmail.com>
2019-10-22 16:16:39 +0545

    added ServiceHistoryitem

--- lib/models/ServiceHistoryItem.dart
+++ lib/models/ServiceHistoryItem.dart
@@ -1,15 +1,26 @@
 class ServiceHistoryItem{
 
-  String Description;
+  String Posting_Date;
+  String Document_No;
+  String Make_Code;
+  String Model_Code;
+  String Vehicle_Serial_No;
+  String Location_Code;
   String No;
-  String Unit_Price;
 
-  ServiceHistoryItem({this.Description, this.No, this.Unit_Price});
+
+  ServiceHistoryItem({this.Posting_Date, this.Document_No,
+  this.Make_Code, this.Model_Code, this.Vehicle_Serial_No, this.Location_Code, this.No});
+
   factory ServiceHistoryItem.fromJson(Map<String, dynamic> json) {
     return ServiceHistoryItem(
-      Description: json['Description'],
+      Posting_Date: json['Posting_Date'],
+      Document_No: json['Document_No'],
+      Make_Code: json['Make_Code'],
+      Model_Code: json['Model_Code'],
+      Vehicle_Serial_No: json['Vehicle_Serial_No'],
+      Location_Code: json['Location_Code'],
       No: json['No'],
-      Unit_Price: json['Unit_Price'],
     );
   }
 }
