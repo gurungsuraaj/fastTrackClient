ceb879aec48eebd7bcbdddc3e10a0c77831de6f4
Pratik Gurung <gurung.pratik04@gmail.com>
2020-01-03 17:32:46 +0545

    search inventory and ui improvement

--- lib/utils/Toast.dart
+++ lib/utils/Toast.dart
@@ -1,3 +1,4 @@
+import 'package:fasttrackgarage_app/utils/ExtraColors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
@@ -6,8 +7,8 @@ class ShowToast {
   static showToast(BuildContext context, String message) {
     Toast.show(message, context,
         duration: Toast.LENGTH_LONG,
-        gravity: Toast.BOTTOM,
-        backgroundColor: Colors.white70,
-        textColor: Colors.black);
+        gravity: Toast.CENTER,
+        backgroundColor: Color(ExtraColors.DARK_BLUE),
+        textColor: Colors.white);
   }
 }
