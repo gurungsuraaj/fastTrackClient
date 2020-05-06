1714a7a764b357159a0c06c517af821ad0d1254b
Suraj Gurung <gurungsooraj21@gmail.com>
2020-05-06 16:28:40 +0545

    home screen

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/surajgurung/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/surajgurung/Documents/FlutterProjects/ebt-fasttrack-devupdated"
-export "FLUTTER_TARGET=/Users/surajgurung/Documents/FlutterProjects/ebt-fasttrack-devupdated/lib/main.dart"
+export "FLUTTER_ROOT=/Users/navrajsharma/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/navrajsharma/Documents/FlutterProject/ebt-fasttrack"
+export "FLUTTER_TARGET=/Users/navrajsharma/Documents/FlutterProject/ebt-fasttrack/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/surajgurung/Desktop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/navrajsharma/Desktop/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
