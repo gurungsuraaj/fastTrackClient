f41606ee8161e95f76d8b27a523f1374fb41264e
Srijana Shrees Magar <shreezana.magar@gmail.com>
2021-09-26 14:18:04 +0545

    other services inquiry ui

--- lib/screens/Inquiries/BatteryInquiry.dart
+++ lib/screens/Inquiries/BatteryInquiry.dart
@@ -521,7 +521,7 @@ class _BatteryInquiryState extends State<BatteryInquiry>
                       shape: RoundedRectangleBorder(
                         borderRadius: new BorderRadius.circular(18.0),
                       ),
-                      primary: Colors.orange,
+                      primary: Color(0xffef773c),
                     ),
                     onPressed: () {
                       // performLogin();
